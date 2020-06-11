2761922a03d6487e406a5d8897d5563f65152193
drewpgilmore@gmail.com <Drew.Gilmore@viasat.com>
2020-06-11 01:14:21 -0700

    calculations working - printing to terminal only

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,13 +2,12 @@
 import os
 import csv
 
-budget_data_path = os.path.join('..','Resources', 'budget_data.csv') #need to be in ../PyBank/Resources directory to access 
-
+budget_data_path = os.path.join('..','Resources', 'budget_data.csv') #needs to be in ../PyBank/Resources directory to access 
 #create empty lists
-months = []
+months = [] 
 profit_loss = []
-
-profitLossRunningTotal = 0
+#set running total to 0 to start
+profitLossRunningTotal = 0 
 
 with open(budget_data_path, encoding="utf8") as PyBankFile:
     csvreader = csv.reader(PyBankFile, delimiter=',')
@@ -23,13 +22,17 @@ Total_Months = len(months)
 Average_Change = round((profitLossRunningTotal / Total_Months), 2)
 Greatest_Increase = max(profit_loss)
 Greatest_Decrease = min(profit_loss)
+Greatest_Increase_Index = profit_loss.index(Greatest_Increase)
+Greatest_Decrease_Index = profit_loss.index(Greatest_Decrease)
+
+
 
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(Total_Months))
-print("Total: " + str(profitLossRunningTotal))
-print("Average Change: " + str(Average_Change))
-print("Greatest Increase: $" + str(Greatest_Increase))
-print("Greatest Decrease: $" + str(Greatest_Decrease))
+print("Total: $" + str(profitLossRunningTotal))
+print("Average Change: $" + str(Average_Change))
+print("Greatest Increase: " + months[Greatest_Increase_Index] + " $" + str(Greatest_Increase))
+print("Greatest Decrease: " + months[Greatest_Decrease_Index] + " $" + str(Greatest_Decrease))
 
 
