8e57cd04b32ae1011b9d6259091baf9f7c623355
drewpgilmore@gmail.com <Drew.Gilmore@viasat.com>
2020-06-16 00:19:47 -0700

    updated script to write to text file as well as terminal. cleaned up print statements

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,36 +1,37 @@
 import os
 import csv
 
-pollPath = os.path.join('..','Resources','election_data.csv')
+pollPath = os.path.join('Resources','election_data.csv')
+output_file = os.path.join('analysis','election_results.txt')
+
+candidates = {}
 
 voterList = []
 candidates = []
 
-with open(pollPath, encoding="utf8") as pollFile:
+with open(pollPath, encoding="utf8") as pollFile, open(output_file, "w") as summaryFile:
     csvReader = csv.reader(pollFile, delimiter=",")
     headers = next(csvReader)
     for row in csvReader:
         voterList.append(row[0])
         candidates.append(row[2])
 
-    print("Elections Results")
-    print("-------------------------")
-    print("Total Votes: " + str(len(voterList)))
-    print("-------------------------")
+    print(f'Elections Results\nTotal Votes: {len(voterList)}\n\n')
+    summaryFile.write(f'Elections Results\nTotal Votes: {len(voterList)}\n\n')
 
     uniqueCandidates = list(set(candidates))
     votes = []
     percentages = []
 
-    for word in uniqueCandidates:
+    for word in uniqueCandidates:       
         voteCount = candidates.count(word)
         votes.append(voteCount)
-        percentage = voteCount/len(candidates)
+        percentage = "{:.2%}".format(voteCount/len(candidates))
         percentages.append(percentage)
-        print(word + ": " + str("{:.2%}".format(percentage)) + " (" + str(voteCount) + ")")
-    
-    print("-------------------------")
-     
+        print(f'{word}: {percentage} ({voteCount})')
+        summaryFile.write(f'{word}: {percentage} ({voteCount})\n')
+
+    print(candidate_summary)
     winner = uniqueCandidates[votes.index(max(votes))]
-    print("Winner: " + winner)
-    print("-------------------------")
+    print(f'\nWinner: {winner}')
+    summaryFile.write(f'\nWinner: {winner}')
