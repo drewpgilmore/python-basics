b48c60911a3b66f1e17e96de0283754ae39b2262
drewpgilmore@gmail.com <drewpgilmore@gmail.com>
2020-10-20 12:44:10 -0700

    modified text file output string

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,7 +11,7 @@ profit_loss = []
 monthlyChange = []
 
 #set running total to 0 to start
-profitLossRunningTotal = 0 
+running_total = 0 
 
 #loop throuh rows in budget data csv file
 with open(budget_data_path, encoding="utf8") as PyBankFile:
@@ -22,7 +22,7 @@ with open(budget_data_path, encoding="utf8") as PyBankFile:
     for row in csvreader:
         months.append(row[0])
         profit_loss.append(int(row[1]))
-        profitLossRunningTotal += int(row[1])
+        running_total += int(row[1])
 
 #initiate while loop to make list of monthly changes
 i = 0
@@ -31,19 +31,19 @@ while i < len(profit_loss)-1:
     i = i + 1
 
 #set result calculations
-Total_Months = len(months) #number of records in list
-Average_Change = round((sum(monthlyChange)/len(monthlyChange)),2) #takes sum of all values in monthly change and divides that by number of values
-Greatest_Increase = max(monthlyChange) #highest value in monthly change list
-Greatest_Decrease = min(monthlyChange) #lowest value in monthly change list
-Greatest_Increase_Index = monthlyChange.index(Greatest_Increase)+1 #offset index number to accomodate for no value corresponding to first month
-Greatest_Decrease_Index = monthlyChange.index(Greatest_Decrease)+1 
+total_months = len(months) #number of records in list
+avg_change = round((sum(monthlyChange)/len(monthlyChange)),2) #takes sum of all values in monthly change and divides that by number of values
+max_increase = max(monthlyChange) #highest value in monthly change list
+max_decrease = min(monthlyChange) #lowest value in monthly change list
+max_increase_index = monthlyChange.index(Greatest_Increase)+1 #offset index number to accomodate for no value corresponding to first month
+greatest_decrease_index = monthlyChange.index(Greatest_Decrease)+1 
 
 #print results to terminal
 print(f'''
     Financial Analysis\n
-    Total Months: {str(Total_Months)}\n
-    Total: ${str(profitLossRunningTotal)}\n
-    Average Change: ${str(Average_Change)}\n 
+    Total Months: {len(months)}\n
+    Total: ${profitLossRunningTotal}\n
+    Average Change: ${avg_change}\n 
     Greatest Increase: {months[Greatest_Increase_Index]} (${str(Greatest_Increase)})\n
     Greatest Decrease: {months[Greatest_Decrease_Index]} (${str(Greatest_Decrease)})
 ''')
@@ -52,13 +52,22 @@ print(f'''
 output_file = os.path.join("analysis","financial_analysis.txt")
 
 #enter results into text file
-with open(output_file, "w") as datafile:
-    datafile.write("Financial Analysis \n")
-    datafile.write("----------------------------\n")
-    datafile.write("Total Months: " + str(Total_Months)+ "\n")
-    datafile.write("Total: $" + str(profitLossRunningTotal)+ "\n" )
-    datafile.write("Average Change: $" + str(Average_Change)+ "\n")
-    datafile.write("Greatest Increase: " + months[Greatest_Increase_Index] + " $" + str(Greatest_Increase)+ "\n")
-    datafile.write("Greatest Decrease: " + months[Greatest_Decrease_Index] + " $" + str(Greatest_Decrease))
+# with open(output_file, "w") as datafile:
+#     datafile.write("Financial Analysis \n")
+#     datafile.write("----------------------------\n")
+#     datafile.write("Total Months: " + str(Total_Months)+ "\n")
+#     datafile.write("Total: $" + str(profitLossRunningTotal)+ "\n" )
+#     datafile.write("Average Change: $" + str(Average_Change)+ "\n")
+#     datafile.write("Greatest Increase: " + months[Greatest_Increase_Index] + " $" + str(Greatest_Increase)+ "\n")
+#     datafile.write("Greatest Decrease: " + months[Greatest_Decrease_Index] + " $" + str(Greatest_Decrease))
 
+with open(output_file, "w") as datafile:
+    datafile.write(f'''Financial Analysis \n
+    ----------------------------\n
+    Total Months: {str(Total_Months)} \n
+    Total: ${str(profitLossRunningTotal)} \n 
+    Average Change: ${str(Average_Change)} \n
+    Greatest Increase: {months[Greatest_Increase_Index]} ${str(Greatest_Increase)} \n
+    Greatest Decrease: {months[Greatest_Decrease_Index]} ${str(Greatest_Decrease)}
+    ''')
 
--- PyPoll/main_pandas.py
+++ PyPoll/main_pandas.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
 file_path = 'Resources/election_data.csv'
-
 election_results_data = pd.read_csv(file_path)
-
 summary_data = election_results_data['Candidate'].value_counts()
-
 print(summary_data)
 
 
